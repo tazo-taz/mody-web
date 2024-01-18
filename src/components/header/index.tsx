336b64e9a61bbc414a6612dc297f68b8b7a643f6
tazo-taz <samfao.tj@gmail.com>
2024-01-18 19:53:08 +0400

    initialize

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import ModyLogoPurple from "../../assets/images/svgs/mody-logo-purple";
+
+export default function Header() {
+    return (
+        <div className='container mx-auto py-[21px]'>
+            <ModyLogoPurple />
+        </div>
+    )
+}
